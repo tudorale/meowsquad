388a7de4002b113e4fa38875f0a18fb9e8e85034
Tudor Alexandru <tudorale2005@gmail.com>
2022-09-21 16:07:50 +0300

    image update

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,11 @@ export default function Home() {
     <>
       <Head>
         <title>Meow Squad</title>
-        <meta name="viewport" />
         <meta name="description" content="Meow Squad is a service that offers advanced tools to assist you in making informed decisions about your NFT investments." />
         <meta name="author" content="Tudor Alexandru -> tudoralexandru.vercel.app" />
         <link rel="icon" href="/favicon.png" />
       </Head>
-      <div className={styles.background}>
-      </div>
+      <img className={styles.background} src="/bg.PNG"/>
     </>
   )
 }
