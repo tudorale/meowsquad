c3cdb6d4c1cc972ee1c54f58a67e38967f17366a
Tudor Alexandru <tudorale2005@gmail.com>
2022-10-01 13:44:53 +0300

    chapter title

--- pages/locations/village.js
+++ pages/locations/village.js
@@ -7,6 +7,8 @@ function village() {
         <img className={styles.image} src="/village-min.png"></img>
 
         <div className={styles.text}>
+            <p className={styles.chapterTitle}>THE BEGINNING OF THE WAY</p>
+
             <p>Welcome to the first location that will open your eyes; Meowvillage. Mostly elderly cats live here. They are engaged in agriculture or, for example, beekeeping like a man in a fountain.</p>
             <p>It is a place full of tranquility, no one is in a hurry, life goes on as usual here. In the morning, slowly, all cats go about their business: adults to work, kids to kindergartens and schools. Just like you and me, isn&apos;t it? There are several kilometers between the village and the nearest town, but this is not a problem, because there is a bus that goes there every hour.</p>
             <p>I am sure each of you would like to be in this very beautiful and magical place right now, and understandably so; being here is like slowing down time. After living in a metropolis where everyone is always in a hurry, Meowvillage will give you a chance to unwind and be yourself.</p>
